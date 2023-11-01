a7fe9f2c280301c6208f973e5a50045dccb81474
marcosviniciusfn <140033522+marcosvfn@users.noreply.github.com>
2023-11-01 18:55:50 -0300

    Update horarios.ts

--- app/natal/horarios.ts
+++ app/natal/horarios.ts
@@ -30,7 +30,7 @@ export const HorariosSabado: HorariosType[] = [
   },
   {
     horario: ["15:00", "15:30"],
-    situacao: "Pré reservado",
+    situacao: "Reservado",
   },
   {
     horario: ["15:40", "16:10"],
